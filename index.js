86d3cb9502112f16e40f112227527ebd46930bee
dibyanshu-pal-kushwaha <dibyanshupkushwaha@gmail.com>
2025-06-28 23:15:48 +0530

    New Repo

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app =express();
+
+app.use(express.json());
+
+function sum(){
+    const a =parseInt(req.body.a);
+    const b =parseInt(req.body.b);
+
+    res.json({
+        sum : a+b
+    })
+
+}
+
+app.post("/sum",sum);
+
+app.listen(3000);
